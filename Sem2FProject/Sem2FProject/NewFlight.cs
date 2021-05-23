304135dd73a4b1228eed0eb05329925e28305eeb
Muhammad-Ali-Murtaza <81867736+Muhammad-Ali-Murtaza@users.noreply.github.com>
2021-05-23 10:45:42 -0700

    All gui from my side

---
+++ Sem2FProject/Sem2FProject/NewFlight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class NewFlight : Form
+    {
+        public NewFlight()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+    }
+        
+}
---
+++ Sem2FProject/Sem2FProject/UpdateFlight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class UpdateFlight : Form
+    {
+        public UpdateFlight()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox4.SelectedIndex = 0;
+            comboBox5.SelectedIndex = 0;
+            comboBox6.SelectedIndex = 0;
+            textBox2.Text = "";
+            textBox3.Text = "";
+            dateTimePicker4.ResetText();
+            dateTimePicker3.ResetText();
+        }
+
+        
+    }
+}
