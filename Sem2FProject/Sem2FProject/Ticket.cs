79d222a8f7d4e90c038dd3b348d78918c40efe61
Zeshan Ayub <unofficialmz@gmail.com>
2021-06-02 13:53:19 +0500

    Ticket and UserData Classes

---
+++ Sem2FProject/Sem2FProject/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem2FProject
+{
+    class Ticket
+    {
+        public int TicketNo
+        {
+            get;set;
+        }
+        public String FlightId
+        {
+            get;set;
+        }
+    }
+}
